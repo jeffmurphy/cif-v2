b6bcf12efdf5e43d5db0c1bbb9a64f9f68157277
jeff murphy <jcmurphy@jeffmurphy.org>
2012-06-27 10:24:57 -0400

    Prep'ing for pull to master

--- src/pb-python/gen-py/cifsupport.py
+++ src/pb-python/gen-py/cifsupport.py
@@ -1,6 +1,21 @@
+import msg_pb2
+import feed_pb2
+
+"""
+Compare the received object's version to the version available
+in our local installation. If the integer portion of the version does 
+not match, throw an exception. The minor (fractional) portion of the
+version is allowed to mismatch.
+
+This will work for both Message and Feed type objects.
+"""
+
+
 def versionCheck(rcvdMsg):
     m = msg_pb2.MessageType()
-#m.version = m.version
+    if rcvdMsg.__class__ == "<class 'feed_pb2.FeedType'>":
+        m = feed_pb2.FeedType()
+        
     if type(rcvdMsg) != type(m):
         raise Exception("Object type mismatch: Recvd=" + str(type(rcvdMsg)) + " != Expected=" + str(type(m)))
     else:
