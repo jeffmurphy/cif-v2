86ca612fe8441abe31bd34e48254c30f84d34e36
jeff murphy <jcmurphy@jeffmurphy.org>
2012-07-11 14:41:22 -0400

    expanded on perl module to include encapsulated message types (iodef, idmef, maec, icsg).

--- src/pb-perl/sbin/generate_perl_bindings.pl
+++ src/pb-perl/sbin/generate_perl_bindings.pl
@@ -3,46 +3,34 @@
 use strict;
 use Google::ProtocolBuffers;
 
-my $f = '../msg.proto';
-Google::ProtocolBuffers->parsefile($f,
-    {
-        generate_code => 'lib/CIF/_Msg.pm',
+# it's important that the final portional of 'packagename' (the part after
+# the final ::) is consistent across languages. Therefor, the package names
+# you see here are the ones that are produced by "protoc" for other languages
+# like Java, Python, etc. They are prefixed by "CIF::" but that will be stripped
+# when creating the message. 
+
+my $todo = [
+    #[ input filename, output filename,  packagename ] 
+	[ 'msg.proto',                'lib/CIF/_Msg.pm',               'CIF::_Msg'              ],
+	[ 'feed.proto',               'lib/CIF/Msg/Feed.pm',           'CIF::Msg::Feed'         ],
+	[ 'ICSG-v1_7-2007.proto',     'lib/ICSG_v1_7_2007_pb2.pm',     'ICSG_v1_7_2007_pb2'     ],
+	[ 'RFC5070-IODEF-v1.proto',   'lib/RFC5070_IODEF_v1_pb2.pm',   'RFC5070_IODEF_v1_pb2'   ],
+	[ 'RFC4765-IDMEF-2007.proto', 'lib/RFC4765_IDMEF_2007_pb2.pm', 'RFC4765_IDMEF_2007_pb2' ],
+	[ 'MAEC-v2.proto',            'lib/MAEC_v2_pb2.pm',            'MAEC_v2_pb2'            ]
+];
+
+foreach my $t (@$todo) {
+	print "Building Perl PB2 module for: ". $t->[0] . "\n";
+	Google::ProtocolBuffers->parsefile(
+		"../" . $t->[0],
+    	{
+        generate_code => $t->[1],
         create_accessors    => 1,
         follow_best_practice => 1,
-    }
-);
-
-$f = '../feed.proto';
-
-Google::ProtocolBuffers->parsefile($f,
-    {
-        generate_code => 'lib/CIF/Msg/Feed.pm',
-        create_accessors    => 1,
-        follow_best_practice => 1,
-    }
-);
-
-# work-around till we fix:
-# https://rt.cpan.org/Ticket/Display.html?id=76641
-
-open(F,'lib/CIF/_Msg.pm') || die($!);;
-my @lines = <F>;
-close(F);
-open(F,'>','lib/CIF/_Msg.pm');
-no warnings;
-print F "package CIF::_Msg;\n";
-foreach (@lines){
-    print F $_;
+        package_name => $t->[2]
+    	}
+	);
 }
-close(F);
 
-open(F,'lib/CIF/Msg/Feed.pm');
-my @lines = <F>;
-close(F);
-open(F,'>','lib/CIF/Msg/Feed.pm');
-no warnings;
-print F "package CIF::Msg::Feed;\n";
-foreach (@lines){
-    print F $_;
-}
-close(F);        
+exit 0;
+     
