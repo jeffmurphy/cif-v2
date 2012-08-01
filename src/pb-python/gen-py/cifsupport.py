f9a7d8e9d35ceaccfc5d22b6139b52222e70292d
jeff murphy <jcmurphy@jeffmurphy.org>
2012-07-31 23:18:17 -0400

    Added control.proto for inter-module communication

--- src/pb-python/gen-py/cifsupport.py
+++ src/pb-python/gen-py/cifsupport.py
@@ -1,5 +1,6 @@
 import msg_pb2
 import feed_pb2
+import control_pb2
 
 """
 Compare the received object's version to the version available
@@ -12,12 +13,31 @@ This will work for both Message and Feed type objects.
 
 
 def versionCheck(rcvdMsg):
+    
     m = msg_pb2.MessageType()
-    if rcvdMsg.__class__ == "<class 'feed_pb2.FeedType'>":
+    if isMessage(rcvdMsg):
+        m = msg_pb2.MessageType()
+    elif isFeed(rcvdMsg):
         m = feed_pb2.FeedType()
-        
-    if type(rcvdMsg) != type(m):
-        raise Exception("Object type mismatch: Recvd=" + str(type(rcvdMsg)) + " != Expected=" + str(type(m)))
+    elif isControl(rcvdMsg):
+        m = control_pb2.ControlType()
     else:
-        if int(m.version) != int(rcvdMsg.version):
-            raise Exception("Version mismatch: Recvd=" + str(int(rcvdMsg.version)) + " != OurIDL=" + str(int(m.version)))
+        raise Exception("Unknown object type. Expected Message/Feed/Control got: " + str(type(m)))
+    
+    if int(m.version) != int(rcvdMsg.version):
+        raise Exception("Version mismatch: Recvd=" + str(int(rcvdMsg.version)) + " != OurIDL=" + str(int(m.version)))
+
+def isMessage(m):
+    if str(m.__class__) == "<class 'msg_pb2.MessageType'>":
+        return 1
+    return 0
+
+def isFeed(m):
+    if str(m.__class__) == "<class 'feed_pb2.FeedType'>":
+        return 1
+    return 0
+
+def isControl(m):
+    if str(m.__class__) == "<class 'control_pb2.ControlType'>":
+        return 1
+    return 0
