92d95543574e371774d38b91af03fc94bfddb37b
jeff murphy <jcmurphy@jeffmurphy.org>
2012-08-01 01:54:31 -0400

    ...

--- src/pb-perl/test.pl
+++ src/pb-perl/test.pl
@@ -2,12 +2,16 @@
 use strict;
 use lib './lib';
 use CIF::Msg;
+use CIF::Msg::Control;
 use CIF::Msg::Support;
 use RFC5070_IODEF_v1_pb2;
 use MAEC_v2_pb2;
 use Data::Dumper;
 
-print "CIF::Msg demo. IDL version: ". CIF::Msg::Support::getOurVersion() . "\n";
+print "CIF::Msg demo.\n";
+print "  Message IDL version: ". CIF::Msg::Support::getOurVersion("Message") . "\n";
+print "  Control IDL version: ". CIF::Msg::Support::getOurVersion("Control") . "\n";
+print "     Feed IDL version: ". CIF::Msg::Support::getOurVersion("Feed") . "\n";
 
 # construct a simple inner message, we'll use MAEC
 
@@ -22,7 +26,7 @@ my $maec = CIF::MAEC::MaecPlaceholder->encode({
 
 my $m = CIF::Msg::MessageType->encode({
 	type => CIF::Msg::MessageType::MsgType::SUBMISSION(),
-	version => CIF::Msg::Support::getOurVersion(),   # _always_ include this when making a message. set to '1' to test version checking code
+	version => CIF::Msg::Support::getOurVersion("Message"),   # _always_ include this when making a message. set to '1' to test version checking code
 	submissionRequest => [ 
 		{
 			baseObjectType => 'MAEC_v2_pb2',
@@ -31,13 +35,30 @@ my $m = CIF::Msg::MessageType->encode({
 	]
 });
 
+my $c = CIF::Msg::ControlType->encode({
+	type => CIF::Msg::ControlType::MsgType::COMMAND(),
+	command => CIF::Msg::ControlType::CommandType::REGISTER(),
+	src => 'me',
+	dst => 'you',
+	version => 20120727, 
+	#version => CIF::Msg::Support::getOurVersion("Control"), # _required_
+});
 
+my $dc = CIF::Msg::ControlType->decode($c);
+
+print $dc->get_version . " \n";
+#
+#if (!CIF::Msg::Support::versionCheck($dc)) {
+#	die "Sorry, version of received message is incompatible. We can not process it.\n" .
+#		"\tOur compiled in version is: " . CIF::Msg::Support::getOurVersion("Control") . "\n" .
+#		"\tRecvd message is version: " . $dc->get_version;
+#}
 
 my $x = CIF::Msg::MessageType->decode($m);
 
 if (!CIF::Msg::Support::versionCheck($x)) {
 	die "Sorry, version of received message is incompatible. We can not process it.\n" .
-		"\tOur compiled in version is: " . getOurVersion() . "\n" .
+		"\tOur compiled in version is: " . CIF::Msg::Support::getOurVersion("Message") . "\n" .
 		"\tRecvd message is version: " . $x->get_version;
 }
 
