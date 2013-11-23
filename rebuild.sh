f95e74fe5eb75287dad6c96b74f2fd3949c10372
Jeff Murphy <jcmurphy@jeffmurphy.org>
2013-11-22 23:04:07 -0500

    ...

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,12 @@
-make clean
+make realclean
+for i in libcif libcif-dbi smrt router db ui ; do
+	cd $i
+	automake --copy --add-missing
+	autoreconf -vf
+	chmod 755 configure
+	cd ..
+done
+
 automake --copy --add-missing
 autoreconf -vf
 chmod 755 configure
