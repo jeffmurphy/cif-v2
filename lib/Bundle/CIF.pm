557ac8ff0894554ff864900147251d15330eacaf
Jeff Murphy <jcmurphy@jeffmurphy.org>
2013-11-25 23:47:44 -0500

    install

---
+++ lib/Bundle/CIF.pm
@@ -0,0 +1,65 @@
+package Bundle::CIF
+
+$VERSION = '1.00';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::CIF - A bundle to install all the required perl modules for CIF
+
+=head1 SYNOPSIS
+
+C<perl -MCPAN -e 'install Bundle::CIF'>
+
+=head1 CONTENTS
+
+Class::Accessor 0.34
+Class::DBI 3.0.17
+Class::Trigger 0.14
+Compress::Snappy 0.18
+Config::Simple 4.59
+Date::Manip 6.11
+DateTime::Format::DateParse 0.05
+DBD::Pg 1.43
+DBI 1.37
+Digest::MD5 2.27
+Digest::SHA 2.10
+Encode 2.23
+File::Spec 0.8
+File::Type 0.22
+Google::ProtocolBuffers 0.08
+Iodef::Pb::Simple 0.10
+Iodef::Pb::Simple 0.17_01
+Linux::Cpuinfo 1.7
+Log::Dispatch 2.32
+LWP::Protocol::https 6.02
+LWP::UserAgent 6.02
+LWPx::ParanoidAgent 1.08_02
+MIME::Base64 3.06
+MIME::Lite 3.027
+Module::Pluggable 3.8
+Module::Pluggable 3.9
+Net::Abuse::Utils 0.12
+Net::Abuse::Utils::Spamhaus 0.03
+Net::DNS::Match 0.04
+Net::Patricia 1.16
+Net::SSLeay 1.43
+OSSP::uuid 1.0501
+Regexp::Common 2.122
+Regexp::Common::net::CIDR 0.02
+Sys::MemInfo 0.91
+Time::HiRes 1.9719
+Try::Tiny 0.04
+URI::Escape 1.56
+
+=head1 DESCRIPTION
+
+CIF required perl modules.
+
+=head1 AUTHOR
+
+Jeff Murphy
+
